42e585da1be015a22168517d4bbcc460e3bf30d0
Lucas Vieira <vieira07lucas@gmail.com>
2022-08-25 11:41:18 -0300

    Estrutura básica do projeto - 2

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
 // Rota para listar os usuários que temos cadastrados.
