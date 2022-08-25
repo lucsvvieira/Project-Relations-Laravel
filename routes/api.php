5cb8622915ac02edf19e9977446676f84600db7e
Lucas Vieira <vieira07lucas@gmail.com>
2022-08-25 11:16:56 -0300

    Estrutura básica do projeto

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Rota para listar os usuários que temos cadastrados.
+Route::get('/users', [UserController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Rota para listar os endereços que temos cadastrados.
+Route::get('/addresses', [AddressController::class, 'index']);
+
